1c449e83d4ab5e7c39ac24210daa917acf53561e
Sarah Brown <esbie.is@gmail.com>
2014-04-26 07:48:57 -0700

    oo player

--- main.lua
+++ main.lua
@@ -1,3 +1,4 @@
+require("player")
 
 function love.load()
   pixelPerMeter = 64
@@ -13,16 +14,9 @@ function love.load()
   }
   ground.body = love.physics.newBody(world, levelWidth/2, levelWidth-ground.h) --remember, the shape (the rectangle we create next) anchors to the body from its center, so we have to move it to (650/2, 650-50/2)
   ground.shape = love.physics.newRectangleShape(ground.w, ground.h) --make a rectangle with a width of 650 and a height of 50
-  ground.fixture = love.physics.newFixture(ground.body, ground.shape); --attach shape to body
+  ground.fixture = love.physics.newFixture(ground.body, ground.shape) --attach shape to body
 
-  player = {
-    h = 10,
-    w = 10
-  }
-  player.body = love.physics.newBody(world, levelWidth/2, levelWidth/2, "dynamic")
-  player.shape = love.physics.newRectangleShape(player.h, player.w)
-  player.fixture = love.physics.newFixture(player.body, player.shape, 1)
-  player.fixture:setRestitution(0.1) --bounce
+  player.load(levelWidth)
 
   love.graphics.setBackgroundColor(200, 200, 248) --set the background color to a nice blue
   love.window.setMode(levelWidth, levelWidth) --set the window dimensions to 650 by 650
@@ -30,15 +24,8 @@ end
 
 function love.update(dt)
   world:update(dt) --this puts the world into motion
-
-  x, y = player.body:getLinearVelocity()
-  velocity = 300
-
-  if love.keyboard.isDown("right") then --press the right arrow key to push the ball to the right
-    player.body:setLinearVelocity(velocity, y)
-  elseif love.keyboard.isDown("left") then --press the left arrow key to push the ball to the left
-    player.body:setLinearVelocity(-velocity, y)
-  end
+  player.update(dt)
+  
 
   if love.mouse.isDown("l") then
     dX = love.mouse.getX() - prevMouseX
@@ -48,23 +35,14 @@ function love.update(dt)
 end
 
 function love.keypressed(key)
-  x, y = player.body:getLinearVelocity()
-  if key == "up" and (y > -10 and y < 10) then
-    player.body:setLinearVelocity(x, -velocity)
-  end
+  player.keypressed(key)
 end
 
 function love.keyreleased(key)
   if key == "escape" then
     love.event.quit()
   end
-
-  x, y = player.body:getLinearVelocity()
-  if key == "right" then --press the right arrow key to push the ball to the right
-    player.body:setLinearVelocity(x/2, y)
-  elseif key == "left" then --press the left arrow key to push the ball to the left
-    player.body:setLinearVelocity(x/2, y)
-  end
+  player.keyreleased(key)
 end
 
 function love.mousepressed( x, y, button )
---
+++ player.lua
@@ -0,0 +1,38 @@
+player = {
+  h = 10,
+  w = 10,
+  velocity = 300
+}
+
+function player.load(levelWidth)
+  player.body = love.physics.newBody(world, levelWidth/2, levelWidth/2, "dynamic")
+  player.shape = love.physics.newRectangleShape(player.h, player.w)
+  player.fixture = love.physics.newFixture(player.body, player.shape, 1)
+  player.fixture:setRestitution(0.1) --bounce
+end
+
+function player.update(dt)
+  x, y = player.body:getLinearVelocity()
+
+  if love.keyboard.isDown("right") then --press the right arrow key to push the ball to the right
+    player.body:setLinearVelocity(player.velocity, y)
+  elseif love.keyboard.isDown("left") then --press the left arrow key to push the ball to the left
+    player.body:setLinearVelocity(-player.velocity, y)
+  end
+end
+
+function player.keypressed(key)
+  x, y = player.body:getLinearVelocity()
+  if key == "up" and (y > -10 and y < 10) then
+    player.body:setLinearVelocity(x, -player.velocity)
+  end
+end
+
+function player.keyreleased( key )
+  x, y = player.body:getLinearVelocity()
+  if key == "right" then --press the right arrow key to push the ball to the right
+    player.body:setLinearVelocity(x/2, y)
+  elseif key == "left" then --press the left arrow key to push the ball to the left
+    player.body:setLinearVelocity(x/2, y)
+  end
+end
